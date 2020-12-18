2a110e2ad137237b7ed6359aaa9803450fb607f2
Edhu Herprabowo <61931509+Muhammad-Ridho-Herprabowo@users.noreply.github.com>
2020-12-18 21:22:39 +0700

    Add files via upload

---
+++ listPenawaran.cs
@@ -0,0 +1,13 @@
+﻿using Promos.Model;
+using System.Collections.Generic;
+
+namespace Kasir
+{
+    internal class listPenawaran
+    {
+        internal static List<Item> ItemSource;
+        internal static object Item;
+
+        public static object Items { get; internal set; }
+    }
+}
